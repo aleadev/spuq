567fc3dc3ba934538b70e23b005702fd88199701
Elmar Zander <e.zander@tu-bs.de>
2011-06-15 01:20:13 +0200

    multiindex_set: fixed it

--- spuq/utils/multiindex_set.py
+++ spuq/utils/multiindex_set.py
@@ -1,41 +1,30 @@
 class MultiindexSet(object):
-    def __init__(self, m, p):
+    def __init__(self, arr):
+        self.arr=arr
+        self.m=arr.shape[1]
+        self.p=max(arr.sum(1))
         pass
+
+    @staticmethod
+    def createCompleteOrderSet(m,p):
+        def createMultiindexSet(m, p):
+            from numpy import int8,  zeros,  vstack, hstack
+            if m==0:
+                return zeros( (1, 0),  int8 )
+            else:
+                I=zeros( (0, m),  int8 )
+                for q in xrange(0, p+1):
+                    J=createMultiindexSet(m-1, q)
+                    Jn=q-J.sum(1).reshape((J.shape[0],1))
+                    I=vstack( (I,  hstack( (J, Jn))))
+                return I
+        arr=createMultiindexSet(m,p)
+        return MultiindexSet(arr)
         
-def createMultiindexSet(m, p,  exact=False):
-    from numpy import int8,  zeros,  vstack, hstack
-    if m==0:
-        if p==0:
-            return zeros( (1, 0),  int8 )
-        else:
-            return zeros( (0, 0),  int8 )
-    else:
-        I=zeros( (0, m),  int8 )
-        if exact:
-            for r in xrange(0, p+1):
-                J=createMultiindexSet(m-1, r,  True)
-                I=vstack( (I,  hstack( (J, (p-r)+zeros( (J.shape[0], 1), int8 )))))
-        else:
-            for q in xrange(0, p+1):
-                for r in xrange(0, q+1):
-                    J=createMultiindexSet(m-1, r,  True)
-                    I=vstack( (I,  hstack( (J, (q-r)+zeros( (J.shape[0], 1), int8 )))))
-        return I
-                
+    def __repr__(self):
+        return "MI(m="+str(self.m)+",p="+str(self.p)+",p="+str(self.arr)+")"
         
 
 def __main__( ):
-    #print "Multiindex: ",  createMultiindexSet( 1,  0)
-    #print "Multiindex: ",  createMultiindexSet( 1,  1)
-    #print "Multiindex: ",  createMultiindexSet( 1,  2)
-    #print "Multiindex: ",  createMultiindexSet( 1,  3)
-    #return
-    #print "Multiindex: ",  createMultiindexSet( 0,  3)
-    #print "Multiindex: ",  createMultiindexSet( 1,  3)
-    print "Multiindex: ",  createMultiindexSet( 3,  3)
-    
+    print "Multiindex: ",  MultiindexSet.createCompleteOrderSet( 3,  3)
 __main__()
-        
-        
-
-        
