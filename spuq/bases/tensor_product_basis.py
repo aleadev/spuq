8eb5bbed22c225c94dd0b4fdd7cf9ab664369e59
Elmar Zander <e.zander@tu-bs.de>
2011-06-15 00:32:28 +0200

    added some more class stubs

---
+++ spuq/bases/tensor_product_basis.py
@@ -0,0 +1,5 @@
+class TensorProductBasis(Basis):
+    def evalAt(self,vector):
+        # for b in bases: 
+        # side question: what about tensor product bases? Is "vector" a tuple then?
+        return NotImplemented
---
+++ spuq/random_fields/random_field.py
@@ -0,0 +1,33 @@
+class RandomField(object):
+    def __init__(self):
+        # maybe this should go into TensorProductBasis
+        self.Phi=SpatialBasis();
+        self.Psi=StochasticBasis();
+        # maybe this should go into derived classes
+        self.coeffs=GMatrix()
+    def __add__(self,other):
+        # add only if same spatial and stochastic basis are used
+        # or add scalar, or spatial function
+        return NotImplemented
+    def __mult__(self,other):
+        # mult by other field
+        # mult by scalar
+        return NotImplemented
+        
+class SeparatedRandomField(RandomField):
+    # e.g. for KL representations
+    pass
+
+class FullRandomField(RandomField):
+    # e.g. for full (PCE,gPC) representations
+    pass
+
+
+class KL(object):
+    pass
+
+class PCE(object):
+    pass
+
+class KL_PCE(object):
+    pass
