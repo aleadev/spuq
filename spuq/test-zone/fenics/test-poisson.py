f21d2787dd4c231ce01b9d25f4fef3852c77c4d3
Martin <eigel@mathematik.hu-berlin.de>
2011-07-15 02:30:55 +0200

    added fenics example demonstrating: *) poisson model problem and mass matrix evaluated with P1 FEM *) interpolation of function on P2 space *) projection onto coarser P1 space

---
+++ spuq/test-zone/fenics/test-poisson.py
@@ -0,0 +1,57 @@
+"""
+Poisson equation with Dirichlet conditions.
+
+-Laplace(u) = f on the unit square.
+u = x*(x-1)*y*(y-1)
+u = u0 = 0 on the boundary.
+f = 2*x*(x-1) + 2*y*(y-1).
+"""
+
+from dolfin import *
+
+# create mesh and define function space
+mesh = UnitSquare(10, 10)
+V = FunctionSpace(mesh, 'CG', 1)
+
+# define boundary conditions
+u0 = Constant(0.0)
+
+def u0_boundary(x, on_boundary):
+    return on_boundary
+
+bc = DirichletBC(V, u0, u0_boundary)
+
+# define variational problem
+v = TestFunction(V)
+u = TrialFunction(V)
+f = Expression('-2*x[0]*(x[0]-1)-2*x[1]*(x[1]-1)')
+a = inner(grad(u), grad(v))*dx
+L = f*v*dx
+
+# compute solution
+A = assemble(a)
+b = assemble(L)
+bc.apply(A,b)
+u = Function(V)
+solve(A,u.vector(),b)
+
+# plot solution and mesh
+#plot(u)
+#plot(mesh)
+#interactive()
+
+# assemble mass matrix and get numpy vector
+M = assemble((u*v)*dx)
+npM = M.array()
+# print 'shape of mass matrix M is ', npM.shape
+
+# interpolation and projection test
+mesh2 = UnitSquare(40,40)
+V2 = FunctionSpace(mesh2, 'CG', 2)
+f2 = Expression('sin(4*3.141*x[0]*x[1])+x[0]*x[1]')
+f2_V2 = interpolate(f2,V2)
+pf2_V1 = project(f2_V2,V)
+
+plot(f2_V2)
+plot(pf2_V1)
+interactive()
