0983b4bff76e58ea9b4d8a444a1840b593edea87
Elmar Zander <e.zander@tu-bs.de>
2011-09-19 11:15:51 +0200

    wip: conf.py for rtd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -94,8 +94,18 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
 #html_theme = 'sphinxdoc'
+#on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#if on_rtd:
+#    html_theme = 'rtd_amend'
+#else:
+#    html_theme = 'sphinxdoc'
+#
+#html_theme = 'default'
+html_theme = 'sphinxdoc'
+
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -125,7 +135,7 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['../source/_static']
-html_style = 'default.css'
+#html_style = 'default.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
