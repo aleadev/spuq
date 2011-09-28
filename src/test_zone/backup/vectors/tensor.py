eb488df03dffe27351568c2d786691a893755c7a
Elmar Zander <e.zander@tu-bs.de>
2011-09-28 23:40:51 +0200

    removed backup directory

--- src/test_zone/backup/vectors/tensor.py
+++
@@ -1,9 +0,0 @@
-class Tensor(GVector):
-    def __init__(self):
-        self.x=1
-    def __add__(self,other):
-        t=Tensor()
-        t.x=self.x+other.x
-        return t
-    def __repr__(self):
-        return str(self.x)
--- src/test_zone/backup/vectors/vector.py
+++
@@ -1,7 +0,0 @@
-class GVector(object):
-    def __add__(self,other):
-        #return GVector()
-        return NotImplemented
-    def __mul__(self,other):
-        return NotImplemented
-  
