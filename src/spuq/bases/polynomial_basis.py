17d0cd8857da3baeed638815eed9f3aac58162bf
Elmar Zander <e.zander@tu-bs.de>
2011-09-29 21:53:05 +0200

    overhaul of basis stuff (wip)

---
+++ src/spuq/bases/polynomial_basis.py
@@ -0,0 +1,54 @@
+from abc import *
+from numpy import array, ones
+
+from spuq.linalg.basis import FunctionBasis
+
+class StochasticBasis(FunctionBasis):
+    __metaclass__ = ABCMeta
+    
+    @abstractmethod
+    def sample(self, n):
+        return NotImplemented
+
+class MultiindexBasis(StochasticBasis):
+    def __init__(self, I,  rvs):
+        assert(I.m == len(rvs))
+        self.I = I
+        self.rvs = rvs
+
+    def sample(self, n):
+        from numpy import array, ones
+        S = ones((self.I.count, n))
+        for i, rv in enumerate(self.rvs):
+            theta = rv.sample(n)
+            Phi = rv.getOrthogonalPolynomials()
+            Q = zeros((self.I.p + 1, n))
+            for q in xrange(self.I.p + 1):
+                Q[q, :] = Phi.eval(q, theta)
+            S = S * Q[self.I.arr[:, i], :]
+        return S
+    pass
+
+class PCBasis(StochasticBasis):
+    # we already have that (normalised or not?)
+    pass
+
+
+class GPCBasis(StochasticBasis):
+    def __init__(self, dist):
+        self._dist = dist
+
+    @property
+    def dist(self):
+        return _dist
+
+    def sample(self, n):
+        S = ones((self.I.count, n))
+        for i, rv in enumerate(self.rvs):
+            theta = dist.sample(n)
+            Phi = dist.getOrthogonalPolynomials()
+            Q = zeros((self.I.p + 1, n))
+            for q in xrange(self.I.p + 1):
+                Q[q, :] = Phi.eval(q, theta)
+            S = S * Q[self.I.arr[:, i], :]
+        return S
---
+++ src/spuq/bases/tests/test_polynomial_basis.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy.testing import *
+
+from spuq.bases.polynomial_basis import *
+
+class TestPolynomialBasis(TestCase):
+    
+    def test_foo(self):
+        pass
+    
+
+
+if __name__ == "__main__":
+    from spuq.utils.multiindex_set import *
+    I = MultiindexSet.createCompleteOrderSet(2, 4)
+    print I
+
+    from spuq.statistics import *
+    N = NormalDistribution()
+    U = UniformDistribution()
+    print N, U
+
+    gpc1 = GPCBasis(I, [N, U])
+    gpc2 = GPCBasis(I, [N, N])
+    print gpc1.sample(3)
+    s1 = gpc1.sample(100)
+    s2 = gpc2.sample(100)
+    from spuq.utils.plot.plotter import Plotter
+    Plotter.figure(1)
+    Plotter.scatterplot(I.arr[:7, :], s1)
+    Plotter.figure(2)
+    Plotter.scatterplot(I.arr[8:15, :], s2)
+    Plotter.figure(3)
+    Plotter.histplot(s1[:6, :], bins=50)
