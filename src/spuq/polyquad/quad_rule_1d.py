84768b1dd017abd92cae3b9c74cae2136d7243b3
Elmar Zander <e.zander@tu-bs.de>
2011-10-16 14:21:29 +0200

    modified file structure for quad rules

--- src/spuq/polyquad/quad_rule_1d.py
+++ src/spuq/polyquad/quad_rule_1d.py
@@ -1,14 +1,29 @@
-#from numpy import array, ndarray, zeros, ones, diag, arange
+from abc import ABCMeta, abstractmetho
+
+from numpy import array, ndarray, zeros, ones, diag, arange, sqrt
+from numpy import linalg as la
 
 class Quad_Rule_1d(object):
     """Abstract base class for 1d quadrature rules on [0,1]"""
+    __metaclass__ = ABCMeta
     
-    @staticmethod
-    def getPointsWeights(np):
+    @abstractmethod
+    def getPointsWeights(self, np):
         return NotImplemented
 
-    @staticmethod
-    def transform(p,interval):
+    def transform(p, interval):
         """Transform points p from [0,1] to interval"""
         return (interval[1]-interval[0])*p + interval[0]
+
+
+class Quad_Rule_Gauss(Quad_Rule_1d):
+    """Gauss quadrature rule"""
     
+    def getPointsWeights(self, np):
+        g = arange(1,10)/sqrt(4.*arange(1,10)**2-ones(9))
+        p,V = la.eig(diag(g,k=1) + diag(g,k=-1))
+        w = 2*V[0,:]**2
+        # transform to [0,1]
+        p = .5 * p + .5
+        w = .5 * w
+        return p,w
---
+++ src/spuq/polyquad/tests/test_quad_rule_1d.py
@@ -0,0 +1,8 @@
+from spuq.utils.testing import *
+from spuq.polyquad.quad_1d import *
+
+class TestQuadRuleGauss(unittest.TestCase):
+    def test_gauss(self):
+        p,w = QuadRuleGauss.getPointsWeights(10)
+        #print p,w
+        assert_almost_equal(sum(w), 1)
