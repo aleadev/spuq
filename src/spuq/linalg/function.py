6bf1bec025206549f68afa9dcdb77383b77834c0
Elmar Zander <e.zander@tu-bs.de>
2012-01-18 20:44:05 +0100

    wip: fixed some bugs in functions plus more test cases

--- src/spuq/linalg/function.py
+++ src/spuq/linalg/function.py
@@ -16,20 +16,20 @@ class GenericFunction(object):
 
     __metaclass__ = ABCMeta
 
-    def __init__(self, domain_dim=1, codomain_dim=1, factor=1, domain=(-infty,infty)):
+    def __init__(self, domain_dim=1, codomain_dim=1, factor=1, domain=(-infty, infty)):
         self.domain_dim = domain_dim
         self.codomain_dim = codomain_dim
         self.factor = factor
         self._domain = [domain for _ in range(self.domain_dim)]
 
     def __call__(self, *x):
-        if len(x)==1 and isinstance(x[0], GenericFunction):
+        if len(x) == 1 and isinstance(x[0], GenericFunction):
             return _compose(self, x[0])
-        elif len(x)==1 and isinstance(x[0], (tuple, list)):
-            assert len(x[0])==self.domain_dim
+        elif len(x) == 1 and isinstance(x[0], (tuple, list)):
+            assert len(x[0]) == self.domain_dim
             return self.eval(*x[0])
         else:
-            assert len(x)==self.domain_dim
+            assert len(x) == self.domain_dim
             return self.eval(*x)
 
     @abstractmethod
@@ -37,11 +37,11 @@ class GenericFunction(object):
         """function evaluation
            return evaluated function at x or function string if x in not set"""
         return NotImplemented
-        
+
     @property
     def D(self):
         "return derivative Df = f.D() = f.diff()"
-        if not hasattr(self,"_diff"):
+        if not hasattr(self, "_diff"):
             self._diff = self.diff()
         return self._diff
 
@@ -54,131 +54,127 @@ class GenericFunction(object):
         return self._domain
 
     def __add__(self, g):
-        return _add(self, g)
+        return _BinaryFunction(self, g, "add")
 
     def __radd__(self, g):
-        return _add(ConstFunction(g, domain_dim=self.domain_dim, codomain_dim=self.codomain_dim), self)
+        return _BinaryFunction(g, self, "add")
 
     def __sub__(self, g):
-        return _add(self, g, sign=-1)
+        return _BinaryFunction(self, g, "sub")
 
     def __rsub__(self, g):
-        return _add(ConstFunction(-g, domain_dim=self.domain_dim, codomain_dim=self.codomain_dim), self)
+        return _BinaryFunction(g, self, "sub")
 
     def __mul__(self, g):
-        return _mul(self, g)
+        return _BinaryFunction(self, g, "mul")
 
     def __rmul__(self, g):
-        return _mul(ConstFunction(g, domain_dim=self.domain_dim, codomain_dim=self.codomain_dim), self)
+        return _BinaryFunction(g, self, "mul")
 
     def __div__(self, g):
-        return _mul(self, g, dodiv=True)
+        return _BinaryFunction(self, g, "div")
 
     def __rdiv__(self, g):
-        return _mul(ConstFunction(g, domain_dim=self.domain_dim, codomain_dim=self.codomain_dim), self, dodiv=True)
+        return _BinaryFunction(g, self, "div")
 
     def __pow__(self, g):
-        return _pow(self, g)
+        return _BinaryFunction(self, g, "pow")
 
-#    def __rpow__(self, g):
-#        return _pow(ConstFunction(g, domain_dim=self.codomain_dim, codomain_dim=self.codomain_dim), self)
+    def __rpow__(self, g):
+        return _BinaryFunction(g, self, "pow")
 
     def __mod__(self, g):
         return _tensorise(self, g)
-        
+
     def __lshift__(self, g):
         return _compose(self, g)
 
 
-def _add(f, g, sign=1):
-    class AddedFunction(GenericFunction):
-        @wrapConstFunction
-        def __init__(self, f, g):
-            assert f.domain_dim == g.domain_dim
-            assert f.codomain_dim == g.codomain_dim
-            GenericFunction.__init__(self, f.domain_dim, f.codomain_dim)
-            self.f = f
-            self.g = g
-        def eval(self, *x):
-            return self._eval(*x)
+def _make_const_func(c, f):
+    """Makes a ConstantFunction from c matching the domain and codomain of f"""
+    return ConstFunction(c, f.domain_dim, f.codomain_dim)
+
+
+def _make_matching_funcs(f, g):
+    """Makes a ConstantFunction from c matching f, if c is not a GenericFunction; otherwise returns c unchanged"""
+    if isinstance(f, GenericFunction):
+        if isinstance(g, GenericFunction):
+            return (f, g)
+        else:
+            return (f, _make_const_func(g, f))
+    else:
+        assert isinstance(g, GenericFunction)
+        return (_make_const_func(f, g), g)
+
+
+class _BinaryFunction(GenericFunction):
+    def __init__(self, f, g, op):
+        (f, g) = _make_matching_funcs(f, g)
+        assert f.domain_dim == g.domain_dim
+        assert f.codomain_dim == g.codomain_dim
+        GenericFunction.__init__(self, f.domain_dim, f.codomain_dim)
+        self.f = f
+        self.g = g
+        self.def_eval(op)
+
+    def eval(self, *x):
+        return self._eval(*x)
+
     def _fadd(self, *x):
         return self.f(*x) + self.g(*x)
     def _Dfadd(self):
         return self.f.diff() + self.g.diff()
+
     def _fsub(self, *x):
-        return self.f(*x) + self.g(*x)
+        return self.f(*x) - self.g(*x)
     def _Dfsub(self):
-        return self.f.diff() + self.g.diff()
+        return self.f.diff() - self.g.diff()
 
-    AF = AddedFunction(f, g)
-    assert abs(sign)==1
-    if sign == 1:
-        AF._eval = MethodType(_fadd, AF, AddedFunction)
-        AF.diff = MethodType(_Dfadd, AF, AddedFunction)
-    else:
-        AF._eval = MethodType(_fsub, AF, AddedFunction)
-        AF.diff = MethodType(_Dfsub, AF, AddedFunction)
-    return AF
-    
-def _mul(f, g, dodiv=False):
-    class MultipliedFunction(GenericFunction):
-        @wrapConstFunction
-        def __init__(self, f, g):
-            assert f.domain_dim == g.domain_dim
-            assert f.codomain_dim == g.codomain_dim
-            GenericFunction.__init__(self, f.domain_dim, f.codomain_dim)
-            self.f = f
-            self.g = g
-        def eval(self, *x):
-            return self._eval(*x)
     def _fdiv(self, *x):
-        return self.f(*x)/self.g(*x)
+        return self.f(*x) / self.g(*x)
     def _Dfdiv(self):
-        return (self.f.diff()*self.g + self.f*self.g.diff())/(self.g**2)
-    def _fmul(self, *x):
-        return self.f(*x)*self.g(*x)
-    def _Dfmul(self):
-        return self.f.diff()*self.g + self.f*self.g.diff()
-    
-    MF = MultipliedFunction(f, g)
-    if dodiv:
-        MF._eval = MethodType(_fdiv, MF, MultipliedFunction)
-        MF.diff = MethodType(_Dfdiv, MF, MultipliedFunction)
-    else:
-        MF._eval = MethodType(_fmul, MF, MultipliedFunction)
-        MF.diff = MethodType(_Dfmul, MF, MultipliedFunction)
-    return MF        
+        return (self.f.diff() * self.g + self.f * self.g.diff()) / (self.g ** 2)
 
-def _pow(f, g):
-    class PowerFunction(GenericFunction):
-        def __init__(self, f, g):
-            assert isinstance(g, (int,float)) or f.codomain_dim == g.codomain_dim
-            if isinstance(g, (int,float)):
-                GenericFunction.__init__(self, f.domain_dim, f.codomain_dim)
+    def _fmul(self, *x):
+        return self.f(*x) * self.g(*x)
+    def _Dfmul(self):
+        return self.f.diff() * self.g + self.f * self.g.diff()
+
+    def _fpow(self, *x):
+        return self.f(*x) ** self.g(*x)
+    def _Dfpow(self, *x):
+        return self._Dpowconst() + self._Dconstpow()
+    def _Dfpowconst(self):
+        return self.g * self.f.D * (self.f ** (self.g - 1))
+    def _Dfconstpow(self):
+        # log of function not yet defined
+        assert False
+        #return self.g.D * log(self.f) * (self.f ** self.g)
+
+    def def_eval(self, op):
+        if op == "add":
+            self._eval = self._fadd
+            self.diff = self._Dfadd
+        elif op == "sub":
+            self._eval = self._fsub
+            self.diff = self._Dfsub
+        elif op == "mul":
+            self._eval = self._fdiv
+            self.diff = self._Dfdiv
+        elif op == "div":
+            self._eval = self._fmul
+            self.diff = self._Dfmul
+        elif op == "pow":
+            self._eval = self._fpow
+            if isinstance(self.f, ConstFunction):
+                self.diff = self._Dfconstpow
+            elif isinstance(self.g, ConstFunction):
+                self.diff = self._Dfpowconst
             else:
-                GenericFunction.__init__(self, f.domain_dim+g.domain_dim, f.codomain_dim)
-            self.f = f
-            self.g = g
-        def eval(self, *x):
-            return self._eval(*x)
-    def _powconst(self, *x):
-        return self.f(*x)**self.g
-    def _Dpowconst(self, *x):
-        return self
-    def _powfunc(self, *x):
-#        print 'split x ',x[:self.f.domain_dim],x[-self.g.domain_dim:]
-        return self.f(x[:self.f.domain_dim])**self.g(x[-self.g.domain_dim:])
-    def _Dpowfunc(self, *x):
-        return self.g.diff()*self
-    
-    PF = PowerFunction(f, g)
-    if isinstance(g, (int,float)):
-        PF._eval = MethodType(_powconst, PF, PowerFunction)
-        PF.diff = MethodType(_Dpowconst, PF, PowerFunction)
-    else:
-        PF._eval = MethodType(_powfunc, PF, PowerFunction)
-        PF.diff = MethodType(_Dpowfunc, PF, PowerFunction)
-    return PF
+                self.diff = self._Dfpow
+        else:
+            assert False
+
 
 def _compose(f, g):
     class ComposedFunction(GenericFunction):
@@ -190,25 +186,25 @@ def _compose(f, g):
         def eval(self, *x):
             return self.f(self.g(x))
         def diff(self):
-            return self.f.D(self.g)*self.g.D
-        
-    return ComposedFunction(f,g)
+            return self.f.D(self.g) * self.g.D
+
+    return ComposedFunction(f, g)
 
 def _tensorise(f, g):
     class TensorisedFunction(GenericFunction):
         def __init__(self, f, g):
             assert f.codomain_dim == g.codomain_dim
-            GenericFunction.__init__(self, f.domain_dim+g.domain_dim, 
+            GenericFunction.__init__(self, f.domain_dim + g.domain_dim,
                                      f.codomain_dim)
             self.f = f
             self.g = g
         def eval(self, *x):
             x1 = x[:self.f.domain_dim]
             x2 = x[self.f.domain_dim:]
-            return self.f(*x1)*self.g(*x2)
+            return self.f(*x1) * self.g(*x2)
         def diff(self):
             return TensorisedFunction(self.f.diff(), self.g.diff())
-        
+
     return TensorisedFunction(f, g)
 
 
@@ -216,18 +212,18 @@ class SimpleFunction(GenericFunction):
     """python function interface"""
 
     def __init__(self, f, Df=None, domain_dim=1, codomain_dim=1):
-        GenericFunction.__init__(self, domain_dim=domain_dim, 
+        GenericFunction.__init__(self, domain_dim=domain_dim,
                                  codomain_dim=codomain_dim)
         self._f = f
         self._Df = Df
-        
+
     def eval(self, *x):
         return self._f(*x)
-    
+
     def diff(self):
         assert self._Df != None
-        return SimpleFunction(self._Df, domain_dim=self.domain_dim, 
-                                  codomain_dim=self.codomain_dim*self.domain_dim)
+        return SimpleFunction(self._Df, domain_dim=self.domain_dim,
+                                  codomain_dim=self.codomain_dim * self.domain_dim)
 
 
 class ConstFunction(GenericFunction):
@@ -237,30 +233,9 @@ class ConstFunction(GenericFunction):
 
     def eval(self, *x):
         if self.codomain_dim > 1:
-            return self.const*ones((self.codomain_dim,1))
+            return self.const * ones((self.codomain_dim, 1))
         else:
             return self.const
 
     def diff(self):
-        return ConstFunction(const=0, domain_dim=self.domain_dim, codomain_dim=self.domain_dim*self.codomain_dim)
-
-def wrapConstFunction(f):
-    def wrap_f(*args):
-        def check_const(a, F):
-            if isinstance(a, (int, float)):
-                return ConstFunction(a, F.domain_dim, F.codomain_dim)
-            else:
-                return a
-
-        F = [a for a in args if isinstance(a,GenericFunction)];
-
-#        print 'ARGS---',args
-        assert len(F)>0
-#        print 'FUNCTION', F[1]
-#        print 'ARGS check_const---',map(lambda a: check_const(a,F[1]), args)
-
-        if len(F) == len(args):     # all arguments are already GenericFunctions
-            f(*args)
-        else:
-            apply(f,map(lambda a: check_const(a,F[1]), args))
-    return wrap_f
+        return ConstFunction(const=0, domain_dim=self.domain_dim, codomain_dim=self.domain_dim * self.codomain_dim)
--- src/spuq/linalg/tests/test_function.py
+++ src/spuq/linalg/tests/test_function.py
@@ -1,49 +1,68 @@
-from spuq.linalg.function import SimpleFunction
+from spuq.linalg.function import SimpleFunction, ConstFunction
 from spuq.utils.testing import *
 
-class TestFunctions(TestCase):
-    def test_function_operators(self):
-        f = SimpleFunction(f=lambda x: x**2)
-        g = SimpleFunction(f=lambda x: 2*x)
-        print 'A1---',f(3)
-        print 'A2---',f((3))
-        print 'A3---',f(f)(3)
-        print 'A4---',f(f(f))(3)
+f = SimpleFunction(f=lambda x: x ** 2, Df=lambda x: 2 * x)
+g = SimpleFunction(f=lambda x: 2 * x, Df=lambda x: 2)
+h = SimpleFunction(f=lambda x, y: 2 * x + 3 * y ** 3, domain_dim=2)
+c3 = ConstFunction(3)
 
-        # addition and multiplication
-        print 'add/mult'
-        print 'B1---',(f+g)(3)
-        print 'B2---',(f-g)(3)
-        print 'B3---',(f+1)(3)
-        print 'B4---',(2+f-g)(3)
-        print 'B5---',(2*f+g)(3)
-        print 'B6---',(2*f*g)(3)
-        print 'B7---',(2*f*g/2)(3)
-        print 'B8---',(f/2+f**3)(3)
-        print 'B9---',(f/2*f**2)(3)
-        print 'B10---',(2*f*g/2+f**2)(3)
+def test_function_operators():
+    assert_equal(f(3), 9)
+    assert_equal(g(3), 6)
+    assert_equal(c3(7), 3)
+    assert_equal(h(2, 3), 85)
+    assert_raises(Exception, f, 3, 3)
+    assert_raises(Exception, c3, 3, 3)
+    assert_raises(Exception, h, 1)
+    assert_raises(Exception, h, 1, 2, 3)
 
-        # composition
-        print 'compose'
-        print 'C1---',f(g)(3), 36
-        print 'C2---',g(f)(3), 18
-        
-        # exponentiation
-        print 'exp'
-        print 'D1---',(f**g)(5,7), 350
-        print 'D2---',(f**g)(7,5), 490
-        h1=f**g
-        h2=g**f
-        # FIXME
+    assert_equal(f.D(3), 6)
+    assert_equal(g.D(3), 2)
+    assert_equal(c3.D(3), 0)
+
+def test_function_addition_and_multiplication():
+    # 
+    print 'add/mult'
+    assert_equal((f + g)(3), f(3) + g(3))
+    assert_equal((f - g)(3), f(3) - g(3))
+    assert_equal((f + 2)(3), f(3) + 2)
+    assert_equal((2 + f)(3), 2 + f(3))
+    assert_equal((f - 2)(3), f(3) - 2)
+    assert_equal((2 - f)(3), 2 - f(3))
+
+def foooo():
+
+    assert_equal((2 + f - g)(3), 5)
+    assert_equal((2 * f + g)(3), 24)
+    assert_equal((f * g)(3), 54)
+    assert_equal((2 * f * g)(3), 108)
+    assert_equal((2 * f * g / 2)(3), 54)
+    assert_equal((f / 2 + f ** 3)(3), 4 + 9 ** 3)
+    assert_equal((f / 2.0 + f ** 3)(3), 4.5 + 9 ** 3)
+
+def test_function_compose():
+    # composition
+    assert_equal(f(g)(3), 36)
+    assert_equal(g(f)(3), 18)
+    assert_equal(f(f(f)(3)), 3 ** 8)
+    assert_equal(g(g(g)(3)), 24)
+
+def test_function_exponentiation():
+    assert_equal((f ** g)(3), 9 ** 6)
+    assert_equal((g ** f)(3), 6 ** 9)
+    h1 = f ** g
+    h2 = g ** f
+    # FIXME
 #        print 'D3---',(h1**h2)(5,7,5,7), 350*490
 
-        # tensorisation
-        print 'tensor'
-        print 'E1---',(f%g)(5,7), 350
-        print 'E2---',(f%g)(7,5), 490
-        h1=f%g
-        h2=g%f
-        print 'E3---',(h1%h2)(5,7,5,7), 350*490
+def foobar():
+    # tensorisation
+    print 'tensor'
+    print 'E1---', (f % g)(5, 7), 350
+    print 'E2---', (f % g)(7, 5), 490
+    h1 = f % g
+    h2 = g % f
+    print 'E3---', (h1 % h2)(5, 7, 5, 7), 350 * 490
 
 
 #    def test_function_vectorisation(self):
