0dabc2bf243d4fa17f399d11bf56faf569e4106e
Elmar Zander <e.zander@tu-bs.de>
2012-02-01 17:53:46 +0100

    [wip] added multi_vector with projection

--- src/spuq/application/egsz/multi_vector.py
+++ src/spuq/application/egsz/multi_vector.py
@@ -1,11 +1,12 @@
-import numpy as np
+from weakref import WeakValueDictionary
 
 from spuq.linalg.vector import Scalar, Vector
 from spuq.math_utils.multiindex import Multiindex
 from spuq.math_utils.multiindex_set import MultiindexSet
-from spuq.utils.type_check import *
+from spuq.utils.type_check import takes, anything, optional
+from spuq.utils import strclass
 
-__all__ = ["MultiVector"]
+__all__ = ["MultiVector", "MultiVectorWithProjection"]
 
 class MultiVector(Vector):
     """Accommodates tuples of type (MultiindexSet, Vector/Object).
@@ -15,9 +16,10 @@ class MultiVector(Vector):
     Note that the type of the second value of the tuple is not restricted to
     anything specific."""
 
-    @takes(anything)
-    def __init__(self):
+    @takes(anything, optional(callable))
+    def __init__(self, on_modify=lambda : None):
         self.mi2vec = dict()
+        self.on_modify = on_modify
 
     @property
     def basis(self):  # pragma: no cover
@@ -39,6 +41,7 @@ class MultiVector(Vector):
 
     @takes(anything, Multiindex, Vector)
     def __setitem__(self, mi, val):
+        self.on_modify()
         self.mi2vec[mi] = val
 
     def keys(self):
@@ -47,17 +50,15 @@ class MultiVector(Vector):
     def active_indices(self):
         return sorted(self.keys())
 
-
-    @takes(anything)
     def copy(self):
-        mv = MultiVector()
+        mv = self.__class__()
         for mi in self.keys():
             mv[mi] = self[mi].copy()
         return mv
 
     @takes(anything, MultiindexSet, Vector)
     def set_defaults(self, multiindex_set, init_vector):
-        # initialise
+        self.on_modify()
         for mi in multiindex_set:
             self[Multiindex(mi)] = init_vector.copy()
 
@@ -73,22 +74,78 @@ class MultiVector(Vector):
 
     def __iadd__(self, other):
         assert self.active_indices() == other.active_indices()
+        self.on_modify()
         for mi in self.active_indices():
             self[mi] += other[mi]
         return self
 
     def __isub__(self, other):
         assert self.active_indices() == other.active_indices()
+        self.on_modify()
         for mi in self.active_indices():
             self[mi] -= other[mi]
         return self
 
     def __imul__(self, other):
         assert isinstance(other, Scalar)
+        self.on_modify()
         for mi in self.keys():
             self[mi] *= other
         return self
 
     def __repr__(self):
-        return "MultiVector(" + str(self.mi2vec.keys()) + ")"
-#        return "MultiVector("+str(self.mi2vec)+")"
+        return "<%s keys=%s>" % (strclass(self.__class__), self.mi2vec.keys())
+
+
+
+
+class MultiVectorWithProjection(MultiVector):
+    @takes(anything, optional(callable))
+    def __init__(self, project=None):
+        MultiVector.__init__(self, self.clear_cache)
+        if not project:
+            project = MultiVectorWithProjection.default_project
+        self.project = project
+        self._proj_cache = WeakValueDictionary()
+        self._back_cache = WeakValueDictionary()
+
+    @staticmethod
+    def default_project(vec_src, vec_dest):
+        """Project the source vector onto the basis of the destination vector."""
+        assert hasattr(vec_dest.basis, "project_onto")
+        return vec_dest.basis.project_onto(vec_src)
+
+    def copy(self):
+        mv = MultiVector.copy(self)
+        mv.project = self.project
+        return mv
+
+    def __eq__(self, other):
+        return (MultiVector.__eq__(self, other) and
+                self.project == other.project)
+
+    def clear_cache(self):
+        self._back_cache.clear()
+        self._proj_cache.clear()
+
+    @takes(anything, Multiindex, Multiindex)
+    def get_projection(self, mu_src, mu_dest):
+        """Return projection of vector in multivector"""
+        args = (mu_src, mu_dest, self.project)
+        vec = self._proj_cache.get(args)
+        if not vec:
+            vec = self.project(self[mu_src], self[mu_dest])
+            self._proj_cache[args] = vec
+        return vec
+
+
+    @takes(anything, Multiindex, Multiindex)
+    def get_back_projection(self, mu_src, mu_dest):
+        """Return back projection of vector in multivector"""
+        args = (mu_src, mu_dest, self.project)
+        vec = self._back_cache.get(args)
+        if not vec:
+            vec_prj = self.get_projection(mu_src, mu_dest)
+            vec = self.project(vec_prj, self[mu_src])
+            self._back_cache[args] = vec
+        return vec
--- src/spuq/application/egsz/tests/test_multi_vector.py
+++ src/spuq/application/egsz/tests/test_multi_vector.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from spuq.utils.testing import *
-from spuq.application.egsz.multi_vector import MultiVector
+from spuq.application.egsz.multi_vector import MultiVector, MultiVectorWithProjection
 from spuq.math_utils.multiindex import Multiindex
 from spuq.math_utils.multiindex_set import MultiindexSet, createCompleteOrderSet
 from spuq.linalg.vector import FlatVector
@@ -28,6 +28,12 @@ def test_keys():
     assert_equal(sorted(keys), sorted([mi1, mi2]))
     inds = mv.active_indices()
     assert_equal(sorted(inds), sorted([mi1, mi2]))
+
+def test_repr():
+    mv = MultiVector()
+    mv[Multiindex([1, 2, 3])] = FlatVector([3, 4, 5])
+    mv[Multiindex([0, 1, 2, 3])] = FlatVector([3, 4, 6])
+    assert_equal(str(mv), "<MultiVector keys=[<Multiindex inds=[0 1 2 3]>, <Multiindex inds=[1 2 3]>]>")
 
 def test_copy():
     # make sure the vectors are copied and each have their own copy of
@@ -122,6 +128,47 @@ def test_sub():
     mv3 = mv1 - mv2
     assert_equal(mv3[mi1], FlatVector([-3, -4, -5]))
     assert_equal(mv3[mi2], FlatVector([5, 4, -7]))
+
+
+def test_mvwp_repr():
+    mv = MultiVectorWithProjection()
+    mv[Multiindex([1, 2, 3])] = FlatVector([3, 4, 5])
+    assert_equal(str(mv), "<MultiVectorWithProjection keys=[<Multiindex inds=[1 2 3]>]>")
+
+
+def test_mvwp_copy():
+    # compares equal to copied MultiVectorWP but not to MultiVector 
+    mv0 = MultiVector()
+    mv1 = MultiVectorWithProjection()
+    mis1 = createCompleteOrderSet(3, 4)
+    mv0.set_defaults(mis1, FlatVector([3, 4, 5]))
+    mv1.set_defaults(mis1, FlatVector([3, 4, 5]))
+    mv2 = mv1.copy()
+    assert_equal(mv2, mv1)
+    assert_not_equal(mv2, mv0)
+
+    # compares equal if project methods match, make sure project method is copied 
+    mv3 = MultiVectorWithProjection(project=lambda : None)
+    mv3.set_defaults(mis1, FlatVector([3, 4, 5]))
+    mv4 = mv3.copy()
+    assert_not_equal(mv2, mv3)
+    assert_equal(mv4, mv3)
+
+def test_mvwp_project():
+    def pr(src, dest):
+        return 2 * src + dest
+    mv1 = MultiVectorWithProjection(project=pr)
+    mi1 = Multiindex([1, 2, 1])
+    mi2 = Multiindex([3, 2, 1, 7])
+    v1 = FlatVector([7, 10, 6])
+    v2 = FlatVector([2, 6, 13])
+    mv1[mi1] = v1
+    mv1[mi2] = v2
+
+    assert_equal(mv1.get_projection(mi1, mi2), pr(v1, v2))
+    assert_equal(mv1.get_back_projection(mi1, mi2), pr(pr(v1, v2), v1))
+
+    # clearing of the cache needs to tested
 
 
 test_main()
