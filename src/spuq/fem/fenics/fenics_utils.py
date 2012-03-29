e6fc8f6389923cf8ddb4b8d8c2cf1b8b672dcdc2
Elmar Zander <e.zander@tu-bs.de>
2012-03-29 23:39:10 +0200

    [wip] added module with utility functions for fenics

---
+++ src/spuq/fem/fenics/fenics_utils.py
@@ -0,0 +1,82 @@
+from types import NoneType
+from spuq.utils.type_check import takes, anything, optional
+from dolfin import (UnitSquare, FunctionSpace, Expression, interpolate, dx,
+                    inner, nabla_grad, TrialFunction, TestFunction,
+                    assemble, Constant, DirichletBC, Mesh, PETScMatrix,
+                    SLEPcEigenSolver, Function, solve)
+from dolfin.cpp import BoundaryCondition
+
+
+@takes(Expression, FunctionSpace)
+def dirichlet_bc(u0, V):
+    def u0_boundary(x, on_boundary):
+        return on_boundary
+    return DirichletBC(V, u0, u0_boundary)
+
+
+@takes(float, FunctionSpace)
+def constant_dirichlet_bc(u0, V):
+    return dirichlet_bc(Constant(u0))
+
+
+def homogeneous_dirichlet_bc(V):
+    return dirichlet_bc(Constant(0.0))
+
+
+DEFAULT_BC = object()
+
+
+@takes((Expression, Function), FunctionSpace)
+def poisson_bilinear_form(coeff_func, V):
+    """Assemble the discrete problem (i.e. the stiffness matrix)."""
+    # setup problem, assemble and apply boundary conditions
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    a = inner(coeff_func * nabla_grad(u), nabla_grad(v)) * dx
+    return a
+
+
+@takes(anything, (BoundaryCondition, NoneType, object), optional(FunctionSpace))
+def apply_bc(fenics_obj, bc=DEFAULT_BC, V=None):
+    if bc is not None:
+        if bc is DEFAULT_BC:
+            if V is None:
+                V = fenics_obj.function_space()
+            bc = homogeneous_dirichlet_bc(V)
+        bc.apply(fenics_obj)
+    return fenics_obj
+
+
+@takes((Expression, Function), FunctionSpace, (BoundaryCondition, NoneType, object))
+def assemble_poisson_matrix(coeff_func, V, bc=DEFAULT_BC):
+    a = poisson_bilinear_form(coeff_func, V)
+    A = assemble(a)
+    return apply_bc(A, bc)
+
+
+@takes(FunctionSpace)
+def l2_gramian_bilinear_form(V):
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    b = u * v * dx
+    return b
+
+
+@takes(FunctionSpace)
+def assemble_l2_gramian_matrix(V):
+    b = l2_gramian_bilinear_form(V)
+    return assemble(b)
+
+
+@takes((Expression, Function), FunctionSpace, (BoundaryCondition, NoneType, object))
+def rhs_linear_form(coeff_func, V):
+    v = TestFunction(V)
+    L = coeff_func * v * dx
+    return L
+
+
+@takes((Expression, Function), FunctionSpace, (BoundaryCondition, NoneType, object))
+def assemble_rhs(coeff_func, V, bc=DEFAULT_BC):
+    L = rhs_linear_form(coeff_func, V)
+    b = apply_bc(L, bc)
+    return b
