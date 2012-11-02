e2c830f65a70122189df8e2f9a749194d152f8a3
Elmar Zander <e.zander@tu-bs.de>
2012-11-02 16:31:22 +0100

    Complete overhaul of sample_problems

--- src/spuq/application/egsz/sample_problems.py
+++ src/spuq/application/egsz/sample_problems.py
@@ -4,6 +4,7 @@ from math import ceil
 from exceptions import NameError
 from numpy.random import random, shuffle
 from scipy.special import zeta
+from collections import namedtuple
 
 from spuq.application.egsz.coefficient_field import ParametricCoefficientField
 from spuq.application.egsz.multi_vector import MultiVectorWithProjection
@@ -21,7 +22,103 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+
 class SampleProblem(object):
+
+    # Definition of PDE types
+    POISSON = "poisson"
+    NAVIER_LAME = "navier_lame"
+    pde_types = [POISSON, NAVIER_LAME]
+
+    # Definitions for coefficient fields
+    func_defs = dict()
+    func_defs[("cos", 1)] = "A*B*cos(freq*pi*m*x[0])"
+    func_defs[("cos", 2)] = "A*B*cos(freq*pi*m*x[0])*cos(freq*pi*n*x[1])"
+    func_defs[("sin", 1)] = "A*B*sin(freq*pi*(m+1)*x[0])"
+    func_defs[("sin", 2)] = "A*B*sin(freq*pi*(m+1)*x[0])*sin(freq*pi*(n+1)*x[1])"
+    func_defs[("monomials", 1)] = "A*B*pow(x[0],freq*m)"
+    func_defs[("monomials", 2)] = "A*B*pow(x[0],freq*m)*pow(x[1],freq*n)"
+    func_defs[("constant", 1)] = "A*B*1.0"
+    func_defs[("constant", 1)] = "1.0+A-A+B-B"
+    func_defs[("constant", 2)] = func_defs[("constant", 1)]
+
+    old_coeff_types = dict()
+    old_coeff_types["EF-square-cos"] = ("cos", "decay-inf")
+    old_coeff_types["EF-square-sin"] = ("sin", "decay-inf")
+    old_coeff_types["monomials"] = ("monomials", "decay-inf")
+    old_coeff_types["linear"] = ("monomials", "constant")
+    old_coeff_types["constant"] = ("constant", "constant")
+
+    # Defintions for right hand side functiosn
+    defaults = dict()
+    defaults[(NAVIER_LAME, "rhs")] = "zero"
+    defaults[(POISSON, "rhs")] = "constant"
+
+    rhs_defs = dict()
+    rhs_defs[(NAVIER_LAME, "zero")] = Constant((0.0, 0.0))
+    rhs_defs[(POISSON, "zero")] = Constant(0.0)
+    rhs_defs[(POISSON, "constant")] = Constant(1.0)
+
+    # Definitions for boundary conditions
+    DIRICHLET = "dirichlet"
+    NEUMANN = "neumann"
+
+    LEFT = "left"
+    RIGHT = "right"
+    TOP = "top"
+    BOTTOM = "bottom"
+
+    BoundaryDef = namedtuple("BoundaryDef", "type where func")
+
+    boundary_defs = dict()
+    boundary_defs[(NAVIER_LAME, "dirichlet_normal_stress")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant((0.0, 0.0))),
+                                                              BoundaryDef(type=DIRICHLET, where=RIGHT, func=Constant((0.3, 0.0)))]
+    boundary_defs[(NAVIER_LAME, 0)] = boundary_defs[(NAVIER_LAME, "dirichlet_normal_stress")]
+
+    boundary_defs[(NAVIER_LAME, "dirichlet_shear_stress")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant((0.0, 0.0))),
+                                                             BoundaryDef(type=DIRICHLET, where=RIGHT, func=Constant((0.0, 0.3)))]
+    boundary_defs[(NAVIER_LAME, 1)] = boundary_defs[(NAVIER_LAME, "dirichlet_shear_stress")]
+
+    boundary_defs[(NAVIER_LAME, "dirichlet_normal_shear")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant((0.0, 0.0))),
+                                                             BoundaryDef(type=DIRICHLET, where=RIGHT, func=Constant((1.0, 1.0)))]
+    boundary_defs[(NAVIER_LAME, 2)] = boundary_defs[(NAVIER_LAME, "dirichlet_normal_shear")]
+
+    boundary_defs[(NAVIER_LAME, "neumann_shear")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant((0.0, 0.0))),
+                                                      BoundaryDef(type=NEUMANN, where=RIGHT, func=Constant((0.0, 1.0)))]
+    boundary_defs[(NAVIER_LAME, 3)] = boundary_defs[(NAVIER_LAME, "neumann_shear")]
+
+    boundary_defs[(NAVIER_LAME, "dirichlet_top_neumann_normal")] = [BoundaryDef(type=DIRICHLET, where=TOP, func=Constant((0.0, 0.0))),
+                                                                    BoundaryDef(type=NEUMANN, where=RIGHT, func=Constant((1.0, 0.0)))]
+    boundary_defs[(NAVIER_LAME, 4)] = boundary_defs[(NAVIER_LAME, "dirichlet_top_neumann_normal")]
+
+
+
+    boundary_defs[(POISSON, "dirichlet_zero_left")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant(0.0))]
+    boundary_defs[(POISSON, 0)] = boundary_defs[(POISSON, "dirichlet_zero_left")]
+    boundary_defs[(POISSON, "dirichlet_inhomogeneous")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant(0.0)),
+                                                           BoundaryDef(type=DIRICHLET, where=RIGHT, func=Constant(3.0))]
+    boundary_defs[(POISSON, 1)] = boundary_defs[(POISSON, "dirichlet_inhomogeneous")]
+    boundary_defs[(POISSON, "dirichlet_zero_all")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant(0.0)),
+                                                      BoundaryDef(type=DIRICHLET, where=RIGHT, func=Constant(0.0)),
+                                                      BoundaryDef(type=DIRICHLET, where=TOP, func=Constant(0.0)),
+                                                      BoundaryDef(type=DIRICHLET, where=BOTTOM, func=Constant(0.0))]
+    boundary_defs[(POISSON, 2)] = boundary_defs[(POISSON, "dirichlet_zero_all")]
+    boundary_defs[(POISSON, "dirichlet_neumann1")] = [BoundaryDef(type=DIRICHLET, where=LEFT, func=Constant(0.0)),
+                                                      BoundaryDef(type=NEUMANN, where=RIGHT, func=Constant(1.0))]
+    boundary_defs[(POISSON, 3)] = boundary_defs[(POISSON, "dirichlet_neumann1")]
+    boundary_defs[(POISSON, "dirichlet_neumann2")] = [BoundaryDef(type=DIRICHLET, where=TOP, func=Constant(1.0)),
+                                                      BoundaryDef(type=NEUMANN, where=RIGHT, func=Constant(1.0))]
+    boundary_defs[(POISSON, 4)] = boundary_defs[(POISSON, "dirichlet_neumann2")]
+
+    # definitions for random variables
+    UNIFORM = "uniform"
+    NORMAL  = "normal"
+
+    rv_defs = dict()
+    rv_defs[UNIFORM] = lambda i: UniformRV(a= -1, b=1)
+    rv_defs[NORMAL] = lambda i: NormalRV(mu=0.5)
+
+
     @classmethod
     @takes(anything, Mesh, int, optional(int), optional(tuple))
     def setupMeshes(cls, mesh, N, num_refine=0, randref=(1.0, 1.0)):
@@ -64,46 +161,30 @@ class SampleProblem(object):
             w[mu] = setup_vec(mesh)
         return w
 
+
+    @staticmethod
+    def get_decay_start(exp, gamma=1):
+        start = 1
+        while zeta(exp, start) >= gamma:
+            start += 1
+        return start
+
     @classmethod
     #@takes(anything, str, str, optional(dict))
     def setupCF2(cls, functype, amptype, rvtype='uniform', gamma=0.9, decayexp=2, freqscale=1, freqskip=0, N=1, scale=1, dim=2, secondparam=None):
-        if rvtype == "uniform":
-            rvs = lambda i: UniformRV(a= -1, b=1)
-        elif rvtype == "normal":
-            rvs = lambda i: NormalRV(mu=0.5)
-        else:
+        try:
+            rvs = cls.rv_defs[rvtype]
+        except KeyError:
             raise ValueError("Unknown RV type %s", rvtype)
 
-        if functype == "cos":
-            if dim == 1:
-                func = "A*B*cos(freq*pi*m*x[0])"
-            else:
-                func = "A*B*cos(freq*pi*m*x[0])*cos(freq*pi*n*x[1])"
-        elif functype == "sin":
-            if dim == 1:
-                func = "A*B*sin(freq*pi*(m+1)*x[0])"
-            else:
-                func = "A*B*sin(freq*pi*(m+1)*x[0])*sin(freq*pi*(n+1)*x[1])"
-        elif functype == "monomials":
-            if dim == 1:
-                func = "A*B*pow(x[0],freq*m)"
-            else:
-                func = "A*B*pow(x[0],freq*m)*pow(x[1],freq*n)"
-        elif functype == "constant":
-#            func = "A*B*1.0"
-#            func = "(A/A)*(B/B)*1.0"
-            func = "1.0+A+B-A-B"
-        else:
-            raise ValueError("Unknown function type %s", functype)
+        try:
+            func = cls.func_defs[(functype, dim)]
+        except KeyError:
+            raise ValueError("Unknown function type %s for dim %s", functype, dim)
             
-        def get_decay_start(exp, gamma=1):
-            start = 1
-            while zeta(exp, start) >= gamma:
-                start += 1
-            return start
 
         if amptype == "decay-inf":
-            start = get_decay_start(decayexp, gamma)
+            start = SampleProblem.get_decay_start(decayexp, gamma)
             amp = gamma / zeta(decayexp, start)
             logger.info("type is decay_inf with start = " + str(start) + " and amp = " + str(amp))
             ampfunc = lambda i: amp / (float(i) + start) ** decayexp
@@ -138,101 +219,55 @@ class SampleProblem(object):
 
     @classmethod
     @takes(anything, str, optional(dict))
-    def setupCF(cls, cftype, decayexp=2, gamma=0.9, freqscale=1, freqskip=0, N=1, rvtype='uniform', scale=1, dim=2, secondparam=None):
-        if cftype == "EF-square-cos":
-            return cls.setupCF2("cos", "decay-inf", rvtype=rvtype, gamma=gamma, decayexp=decayexp, freqscale=freqscale, freqskip=freqskip, N=N, scale=scale, dim=dim, secondparam=secondparam)
-        elif cftype == "EF-square-sin":
-            return cls.setupCF2("sin", "decay-inf", rvtype=rvtype, gamma=gamma, decayexp=decayexp, freqscale=freqscale, freqskip=freqskip, N=N, scale=scale, dim=dim, secondparam=secondparam)
-        elif cftype == "monomials":
-            return cls.setupCF2("monomials", "decay-inf", rvtype=rvtype, gamma=gamma, decayexp=decayexp, freqscale=freqscale, freqskip=freqskip, N=N, scale=scale, dim=dim, secondparam=secondparam)
-        elif cftype == "linear":
-            return cls.setupCF2("monomials", "constant", rvtype=rvtype, gamma=gamma, N=2, scale=scale, dim=dim, secondparam=secondparam)
-        elif cftype == "constant":
-            return cls.setupCF2("constant", "constant", rvtype=rvtype, gamma=gamma, N=2, scale=scale, dim=dim, secondparam=secondparam)
-        else:
-            raise ValueError('Unsupported function type: %s', cftype)
+    def setupCF(cls, cftype, decayexp=2, gamma=0.9, freqscale=1, freqskip=0, N=2, rvtype='uniform', scale=1, dim=2, secondparam=None):
+        try:
+            func_type, amp_type = cls.old_coeff_types[cftype]
+        except KeyError:
+            raise ValueError('Unsupported coefficient type: %s', cftype)
+        return cls.setupCF2(func_type, amp_type, rvtype=rvtype, gamma=gamma, decayexp=decayexp, freqscale=freqscale, freqskip=freqskip, N=N, scale=scale, dim=dim, secondparam=secondparam)
 
     @classmethod
     @takes(anything, int, str, int)
     def setupPDE(cls, boundary_type, domain_name, problem_type, boundaries, coeff_field):
+        pde_type = cls.pde_types[problem_type]
+
+        # define source term
+        #f = Expression("10.*exp(-(pow(x[0] - 0.6, 2) + pow(x[1] - 0.4, 2)) / 0.02)", degree=3)
+        rhs_type = cls.defaults[(pde_type, "rhs")]
+        f = cls.rhs_defs[(pde_type, rhs_type)]
+
+
+        Dirichlet_boundary = []
+        uD = []
+        Neumann_boundary = []
+        g = []
+
+        try:
+            for bc_def in cls.boundary_defs[(pde_type, boundary_type)]:
+                type, where, func = bc_def
+                if type==cls.NEUMANN:
+                    Neumann_boundary.append(boundaries[where])
+                    g.append(func)
+                elif type==cls.DIRICHLET:
+                    Dirichlet_boundary.append(boundaries[where])
+                    uD.append(func)
+                else:
+                    assert False
+        except KeyError:
+            # keine Ahnung gerade
+            raise
+
         a0 = coeff_field.mean_func
-        Dirichlet_boundary = None
-        uD = None
-        Neumann_boundary = None
-        g = None
         
-        if problem_type == 1:
-            # ========== Navier-Lame ===========
-            # define source term
-            f = Constant((0.0, 0.0))
-
-            # homogeneous Neumann does not have to be set explicitly
-            Neumann_boundary = None # (boundaries['right'])
-            g = None #Constant((0.0, 10.0))
-            
-            # define Dirichlet bc
-            if boundary_type == 0:
-                Dirichlet_boundary = (boundaries['left'], boundaries['right'])
-                uD = (Constant((0.0, 0.0)), Constant((0.3, 0.0)))
-            elif boundary_type == 1:
-                Dirichlet_boundary = (boundaries['left'], boundaries['right'])
-                uD = (Constant((0.0, 0.0)), Constant((0.0, 0.3)))
-            elif boundary_type == 2:
-                Dirichlet_boundary = (boundaries['left'], boundaries['right'])
-                uD = (Constant((0.0, 0.0)), Constant((1.0, 1.0)))
-            elif boundary_type == 3:
-                Dirichlet_boundary = (boundaries['left'])
-                uD = (Constant((0.0, 0.0)))
-                Neumann_boundary = (boundaries['right'])
-                g = (Constant((0.0, 1.0)))
-            elif boundary_type == 4:
-                Dirichlet_boundary = (boundaries['top'])
-                uD = (Constant((0.0, 0.0)))
-                Neumann_boundary = (boundaries['right'])
-                g = (Constant((1.0, 0.0)))
-            else:
-                raise Exception("wrong boundary type %i for problem type %i" % boundary_type, problem_type)
-                    
-            # create pde instance
-            pde = FEMNavierLame(lmbda0=a0[0], mu0=a0[1],
+        if pde_type == cls.NAVIER_LAME:
+            pde = FEMNavierLame(lmbda0=a0[0], mu0=a0[1], f=f,
                                 dirichlet_boundary=Dirichlet_boundary, uD=uD,
-                                neumann_boundary=Neumann_boundary, g=g, f=f)
+                                neumann_boundary=Neumann_boundary, g=g)
+        elif pde_type == cls.POISSON:
+            pde = FEMPoisson(a0=a0, f=f,
+                             dirichlet_boundary=Dirichlet_boundary, uD=uD,
+                             neumann_boundary=Neumann_boundary, g=g)
         else:
-            assert problem_type == 0
-            # ========== Poisson ===========
-            # define source term
-            #f = Expression("10.*exp(-(pow(x[0] - 0.6, 2) + pow(x[1] - 0.4, 2)) / 0.02)", degree=3)
-            f = Constant(1.0)
-            
-            # homogeneous Neumann does not have to be set explicitly
-            Neumann_boundary = None
-            g = None
-
-            # define Dirichlet bc
-            if boundary_type == 0:
-                Dirichlet_boundary = (boundaries['left'])
-                uD = (Constant(0.0))
-            elif boundary_type == 1:
-                Dirichlet_boundary = (boundaries['left'], boundaries['right'])
-                uD = (Constant(0.0), Constant(3.0))
-            elif boundary_type == 2:
-                Dirichlet_boundary = (boundaries['left'], boundaries['right'], boundaries['top'], boundaries['bottom'])
-                uD = (Constant(0.0), Constant(0.0), Constant(0.0), Constant(0.0))
-            elif boundary_type == 3:
-                Dirichlet_boundary = (boundaries['left'])
-                uD = (Constant(0.0))
-                Neumann_boundary = (boundaries['right'])
-                g = (Constant(1.0))
-            elif boundary_type == 4:
-                Dirichlet_boundary = (boundaries['top'])
-                uD = (Constant(1.0))
-                Neumann_boundary = (boundaries['bottom'])
-                g = (Constant(1.0))
-            else:
-                raise Exception("wrong boundary type %i for problem type %i" % boundary_type, problem_type)
-            
-            # create pde instance
-            pde = FEMPoisson(a0=a0, dirichlet_boundary=Dirichlet_boundary, uD=uD,
-                             neumann_boundary=Neumann_boundary, g=g, f=f)
+            assert False
 
         return pde, Dirichlet_boundary, uD, Neumann_boundary, g, f
