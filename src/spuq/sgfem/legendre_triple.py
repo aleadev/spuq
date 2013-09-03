04b88f6d29ca24d1ac9b086f1a9b8cc197cb2835
Elmar Zander <e.zander@tu-bs.de>
2013-09-03 18:16:39 +0200

    Corrected evaluate_triples function (returns ScipyOperator now)

--- src/spuq/sgfem/legendre_triple.py
+++ src/spuq/sgfem/legendre_triple.py
@@ -1,7 +1,12 @@
 from __future__ import division
-import numpy as np
-from scipy.sparse import dok_matrix, lil_matrix, csc_matrix, csr_matrix
+
 import collections
+import numpy as np
+import scipy.sparse as sps
+
+from spuq.linalg.scipy_operator import ScipyOperator
+from spuq.linalg.basis import CanonicalBasis
+
 
 def evaluate_triples(polysys, I_a, I_b):
     # get dimensions
@@ -12,8 +17,8 @@ def evaluate_triples(polysys, I_a, I_b):
     if not isinstance(polysys, collections.Sequence):
         polysys = [polysys] * K
     # prepare sparse matrices
-    L = [dok_matrix((M, N)) for _ in range(K)]
-    for k in range(K):
+    L = [sps.dok_matrix((M, N)) for _ in range(-1,K)]
+    for k in range(-1, K):
         for i, mui in enumerate(I_a):
             for j, muj in enumerate(I_b):
                 # compare indices
@@ -23,11 +28,14 @@ def evaluate_triples(polysys, I_a, I_b):
                     ak, bk = mui[k], muj[k]
                     delta = ak - bk
                     if -1 <= delta <= 1:
-                        beta = polysys[k].get_beta(bk)
-                        val = beta[delta]
+                        if k != -1:
+                            beta = polysys[k].get_beta(bk)
+                            val = beta[delta]
+                        else:
+                            val = 1
                     
                         if val <> 0.0:
-                            L[k][i, j] = val
+                            L[k+1][i, j] = val
     # convert to efficient sparse format
-    L = [l.asformat('csr') for l in L] 
+    L = [ScipyOperator(l.asformat('csr'), domain=CanonicalBasis(N), codomain=CanonicalBasis(M)) for l in L]
     return L
