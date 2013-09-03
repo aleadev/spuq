0b061709714ba69b27d30e9f28531d7bdf4d12d5
Elmar Zander <e.zander@tu-bs.de>
2013-09-03 18:13:24 +0200

    Changed TensorVector to abstract base, implemented MatrixTensorVector

--- src/spuq/sgfem/tensor_vector.py
+++ src/spuq/sgfem/tensor_vector.py
@@ -1,105 +1,97 @@
-from spuq.linalg.vector import Vector, Scalar
-import numpy as np
-
 import logging
+import numpy as np
+import scipy.sparse as sps
+
+from spuq.linalg.vector import Vector, Scalar
+from spuq.linalg.operator import Operator
+from spuq.linalg.basis import Basis, CanonicalBasis
+from spuq.utils.type_check import takes, anything
+
 logger = logging.getLogger(__name__)
 
+class TensorBasis(CanonicalBasis):
+    def __init__(self):
+        super(TensorBasis, self).__init__(-1)
+
+
 class TensorVector(Vector):
-    def __init__(self, V):
-        """Initialise with list of vectors."""
-        self.V = V
-        self.M = len(V)
-        self.I = V[0].dim
+    def __init__(self, basis):
+        self._basis = basis
+
+    @property
+    def basis(self):
+        return self._basis
+
+
+class MatrixTensorVector(TensorVector):
+
+    @takes(anything, np.ndarray, TensorBasis)
+    def __init__(self, X, basis):
+        """Initialise with list of vectors.
+        @param X:
+        @param basis:
+        """
+        TensorVector.__init__(self, basis)
+        self.X = X
     
     def dim(self):  # pragma: no cover
         """Return dimension of this vector"""
-        return self.M*self.I
+        return self.X.shape
 
     def copy(self):  # pragma: no cover
-        raise NotImplementedError
+        return self.__class__(self.X.copy(), self.basis)
 
     def flatten(self):
-        return np.hstack([v.array for v in self.V])
+        return self.X.ravel()
 
     def as_matrix(self):
-        return np.vstack([v.array for v in self.V]).T
+        return self.X
 
-    def basis(self):
-        return [v.basis for v in self.V]
+    @takes(anything, Operator, int)
+    #@takes(anything, (np.ndarray, sps.spmatrix), int)
+    def apply_matrix(self, op, axis):
+        # TODO:
+        A = op.as_matrix()
+        X = np.matrix(self.X)
+        if axis==0:
+            Y = A * X
+        elif axis==1:
+            Y = (A * X.T).T
+        return self.__class__(Y, self._basis)
+        
+    @property
+    def coeffs(self):
+        return self.flatten()
 
-    def __getitem__(self, i):
-        return self.V[i]
-    
-    def __setitem__(self, i, val):
-        self.V[i] = val
 
-    def __len__(self):
-        return len(self.V)
-    
-#     def transpose(self):
-#         pass
-# 
+
     def __eq__(self, other):  # pragma: no cover
         """Test whether vectors are equal."""
-        return np.all([v1==v2 for v1, v2 in zip(self.V, other.V)])
+        return np.all(self.X == other.X)
  
-    def __neg__(self, other):  # pragma: no cover
+    def __neg__(self):  # pragma: no cover
         """Compute the negative of this vector."""
-        return TensorVector([-v for v in self.V])
+        return MatrixTensorVector(-self.X, self.basis)
  
     def __iadd__(self, other):  # pragma: no cover
         """Add another vector to this one."""
-        for i in range(self.M):
-            self.V[i] += other[i]
+        self.X += other.X
+        return self
  
     def __imul__(self, other):  # pragma: no cover
         """Multiply this vector with a scalar."""
-        for i in range(self.M):
-            if isinstance(other, Scalar):
-                self.V[i] *= other
-            else:
-                self.V[i] *= other[i]
- 
-#     def __inner__(self, other):  # pragma: no cover
-#         """Scalar product of this vector with another vector."""
-#         return NotImplemented
-# 
-#     def __rinner__(self, other):  # pragma: no cover
-#         """Scalar product of this vector with another vector (reverse)."""
-#         return NotImplemented
-# 
-#     def __add__(self, other):
-#         """Add two vectors."""
-#         return self.copy().__iadd__(other)
-# 
-#     def __radd__(self, other):  # pragma: no cover
-#         """This happens only when other is not a vector."""
-#         if isinstance(other, Scalar) and other == 0:
-#             return self
-#         return NotImplemented
-# 
-#     def __sub__(self, other):
-#         """Subtract two vectors."""
-#         if hasattr(self, "__isub__"):
-#             return self.copy().__isub__(other)
-#         return self +(-other)
-# 
-#     def __rsub__(self, other):  # pragma: no cover
-#         """This happens only when other is not a vector."""
-#         return NotImplemented
-# 
-#     def __mul__(self, other):
-#         """Multiply a vector with a scalar from the right."""
-#         if isinstance(other, Scalar):
-#             return self.copy().__imul__(other)
-#         return NotImplemented
-# 
-#     def __rmul__(self, other):
-#         """Multiply a vector with a scalar from the left."""
-#         if isinstance(other, Scalar):
-#             return self.copy().__imul__(other)
-#         return NotImplemented
-# 
-#     def __repr__(self):
-#         return "<%s basis=%s, coeffs=%s>" % \
-#                (strclass(self.__class__), self.basis, self.coeffs)
+        if isinstance(other, Scalar):
+            self.X *= other
+            return self
+        else:
+            raise TypeError
+
+    def __inner__(self, other):
+        assert isinstance(other, MatrixTensorVector)
+        return np.sum(np.multiply(self.X, other.X))
+
+    @classmethod
+    def from_list(cls, L):
+        X = np.vstack(L).T
+        print "TTT", X.shape, len(L), type(L[0])
+        return cls(X, TensorBasis())
