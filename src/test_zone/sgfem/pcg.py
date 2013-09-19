6baf52c953d370e50e12c110f80071aa8647fcab
Elmar Zander <e.zander@tu-bs.de>
2013-09-19 21:13:57 +0200

    Reformulated pcg to work with cptensors and wrote some tests for it

--- src/test_zone/sgfem/pcg.py
+++ src/test_zone/sgfem/pcg.py
@@ -3,7 +3,7 @@
 from spuq.utils.type_check import takes, optional, returns, tuple_of
 
 from spuq.linalg.operator import Operator
-from spuq.linalg.vector import Vector, inner
+from spuq.linalg.vector import Vector, inner, Flat
 from spuq.utils.forgetful_vector import ForgetfulVector
 
 __all__ = ["pcg"]
@@ -11,43 +11,78 @@ __all__ = ["pcg"]
 import logging
 logger = logging.getLogger(__name__)
 
+def _identity(x):
+    return x
+
 @takes(Operator, Vector, Operator, Vector, optional(float), optional(int))
-def pcg(A, f, P, w0, eps=1e-4, maxiter=100):
-    # for most quantities in PCG (except zeta) only the most recent 
+def pcg(A, b, P, x0, eps=1e-4, maxiter=100, truncate_func=_identity):
+
+    in_tensor_format = not isinstance(b, Flat)
+
+    # for most quantities in PCG (except rho) only the most recent
     # values needs to be kept in memory
-    w = ForgetfulVector(1)
-    rho = ForgetfulVector(1)
-    s = ForgetfulVector(1)
-    v = ForgetfulVector(1)
+    x = ForgetfulVector(1)
+    r = ForgetfulVector(1)
+    q = ForgetfulVector(1)
     z = ForgetfulVector(1)
-    alpha = ForgetfulVector(1)
-    zeta = ForgetfulVector(2)
+    p = ForgetfulVector(1)
 
-    w[0] = w0
-    rho[0] = f - A * w[0]
-    s[0] = P * rho[0]
-    v[0] = s[0]
-    zeta[0] = inner(rho[0], s[0])
+    qp = ForgetfulVector(1)
+    rho = ForgetfulVector(2)
+    alpha = ForgetfulVector(2)
+    beta = ForgetfulVector(2)
+
+    x[0] = x0
+    r[0] = b - A * x[0]
+    r[0] = truncate_func(r[0])
+    z[0] = P * r[0]
+    p[0] = z[0]
+    rho[0] = inner(r[0], z[0])
+
     for i in xrange(1, maxiter):
-        logger.info("pcg iter: %s -> zeta=%s, rho^2=%s" % (i, zeta[i - 1], inner(rho[i - 1], rho[i - 1])))
-        if zeta[i - 1] < 0:
-            for mu in rho[i - 1].active_indices():
-                print i, mu, inner(rho[i - 1][mu], s[i - 1][mu])
-            raise Exception("Preconditioner for PCG is not positive definite (%s)" % zeta[i - 1])
-        if zeta[i - 1] <= eps ** 2:
-            return (w[i - 1], zeta[i - 1], i)
+        logger.info("pcg iter: %s -> rho=%s, r^2=%s" % (i, rho[i - 1], inner(r[i - 1], r[i - 1])))
+        if rho[i - 1] < 0:
+            for mu in r[i - 1].active_indices():
+                print i, mu, inner(r[i - 1][mu], z[i - 1][mu])
+            raise Exception("Preconditioner for PCG is not positive definite (%s)" % rho[i - 1])
+        if rho[i - 1] <= eps ** 2:
+            return (x[i - 1], rho[i - 1], i)
 
-        z[i - 1] = A * v[i - 1]
-        alpha[i - 1] = inner(z[i - 1], v[i - 1])
-        if alpha[i - 1] == 0:
-            raise Exception("Matrix for PCG is singular (%s)" % alpha[i - 1])
-        elif alpha[i - 1] < 0:
-            raise Exception("Matrix for PCG is not positive definite (%s)" % alpha[i - 1])
+        q[i - 1] = A * p[i - 1]
+        q[i - 1] = truncate_func(q[i - 1])
 
-        w[i] = w[i - 1] + zeta[i - 1] / alpha[i - 1] * v[i - 1]
-        rho[i] = rho[i - 1] - zeta[i - 1] / alpha[i - 1] * z[i - 1]
-        s[i] = P * rho[i]
-        zeta[i] = inner(rho[i], s[i])
-        v[i] = s[i] + zeta[i] / zeta[i - 1] * v[i - 1]
+        qp[i - 1] = inner(q[i - 1], p[i - 1])
+        if qp[i - 1] == 0:
+            raise Exception("Matrix for PCG is singular (%s)" % qp[i - 1])
+        elif qp[i - 1] < 0:
+            raise Exception("Matrix for PCG is not positive definite (%s)" % qp[i - 1])
+
+        alpha[i - 1] = rho[i - 1] / qp[i - 1]
+        x[i] = x[i - 1] + alpha[i - 1] * p[i - 1]
+        x[i] = truncate_func(x[i])
+
+        if in_tensor_format:
+            r[i] = b - A * x[i]
+            r[i] = truncate_func(r[i])
+        else:
+            r[i] = r[i - 1] - alpha[i - 1] * q[i - 1]
+
+        z[i] = P * r[i]
+        z[i] = truncate_func(z[i])
+
+        rho[i] = inner(r[i], z[i])
+        beta[i] = rho[i] / rho[i - 1]
+
+        p[i] = z[i] + beta[i] * p[i - 1]
+        p[i] = truncate_func(p[i])
+
+        if False and in_tensor_format:
+            print x[i].rank
+            print z[i].rank
+            print r[i].rank
+            print p[i].rank
+            print q[i-1].rank
+
+
 
     raise Exception("PCG did not converge")
--- src/test_zone/sgfem/test_pcg_on_tensors.py
+++ src/test_zone/sgfem/test_pcg_on_tensors.py
@@ -1,26 +1,33 @@
+import logging
+
 from scipy.linalg import norm
 import numpy as np
 import scipy.sparse as sps
+
 from spuq.linalg.cptensor import CPTensor
 from spuq.linalg.operator import MultiplicationOperator
 from spuq.linalg.tensor_basis import TensorBasis
-
 from spuq.linalg.tensor_operator import TensorOperator
 from spuq.linalg.tensor_vector import FullTensor
 from spuq.linalg.scipy_operator import ScipyOperator
 from spuq.linalg.basis import CanonicalBasis
 from test_zone.sgfem import pcg
 
+logger = logging.getLogger(__name__)
+
+def generate_matrix(n):
+    A = np.random.rand(n, n) - 0.5
+    return sps.csr_matrix(np.dot(A, A.T))
 
 def construct_operator(n1, n2, R):
     # prepare "deterministic" matrices
     basis1 = CanonicalBasis(n1)
     basis2 = CanonicalBasis(n2)
 
-    A1 = [ScipyOperator(sps.csr_matrix(np.random.rand(n1, n1)),
+    A1 = [ScipyOperator(generate_matrix(n1),
                         domain=basis1, codomain=basis1)
           for _ in range(R)]
-    A2 = [ScipyOperator(sps.csr_matrix(np.random.rand(n2, n2)),
+    A2 = [ScipyOperator(generate_matrix(n2),
                         domain=basis2, codomain=basis2)
           for _ in range(R)]
 
@@ -36,17 +43,6 @@ def construct_cptensor(n1, n2, R):
     X2 = np.random.rand(n2, R)
     return CPTensor([X1, X2], basis)
 
-
-# test tensor operator application
-# ================================
-n1 = 100
-n2 = 15
-R = 5
-
-A = construct_operator(n1, n2, R)
-u = construct_cptensor(n1, n2, 5)
-
-
 def construct_preconditioner(A):
     basis = A.domain
     basis1 = basis[0]
@@ -57,11 +53,39 @@ def construct_preconditioner(A):
         [MultiplicationOperator(1, domain=basis2)],
         basis)
 
+def rank_truncate(R_max):
+    """Create a truncation function that truncates by rank"""
+    def do_truncate(X):
+        return X.truncate(R_max)
+    return do_truncate
 
+def test_solve_pcg(A, P, u, f, **kwargs):
+    """Solve the linear problem with given solution and show solve statistics"""
+    [u2, zeta, iter] = pcg.pcg(A, f, P, 0*u, **kwargs)
+
+    logger.info("error:  %s",  np.linalg.norm(u.flatten().as_array() - u2.flatten().as_array()))
+    logger.info("iter:   %s",  iter)
+    logger.info("norm_u: %s",  np.linalg.norm(u.flatten().as_array()))
+    logger.info("norm_f: %s",  np.linalg.norm((A*u).flatten().as_array()))
+
+# test tensor operator application
+# ================================
+np.random.seed(10)
+
+n1 = 100
+n2 = 15
+R_A = 5
+R_u = 4
+
+A = construct_operator(n1, n2, R_A)
 P = construct_preconditioner(A)
+
+u = construct_cptensor(n1, n2, R_u)
 u_flat = u.flatten()
-f_flat = A * u_flat
 
-u_flat2 = pcg.pcg(A, f_flat, P, 0*u_flat)
+#pcg.logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO)
 
-print np.linalg.norm(u_flat.as_array() - u_flat2.as_array())
+eps = 0.3
+test_solve_pcg(A, P, u_flat, A * u_flat, eps=eps)
+test_solve_pcg(A, P, u, A * u, truncate_func=rank_truncate(10), eps=eps)
