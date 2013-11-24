d6f9849bb984c7c14c6e150105552f3b2a707dee
Martin Eigel <Martin Eigel>
2013-11-24 18:07:57 +0100

    reference setup generation

---
+++ src/spuq/application/egsz/generate_reference_setup.py
@@ -0,0 +1,32 @@
+from dolfin import mesh
+from spuq.fem.fenics.fenics_utils import create_joint_mesh
+
+import pickle
+import os
+import glob
+
+
+def generate_reference_setup(basedir, SOLUTIONFN='SFEM2-SOLUTIONS-P?.pkl'):
+    # ============================================================
+    # import solutions and determine reference setting
+    # ============================================================
+    for i, fn in enumerate(glob.glob(os.path.join(basedir, SOLUTIONFN))):
+        print "--------> loading data from", fn
+        with open(fn, 'rb') as fin:
+            w_history = pickle.load(fin)
+            mesh0 = w_history[-1].basis.basis.mesh
+            Lambda0 = w_history[-1].active_indices()
+            print "\tmesh has %i cells and Lambda is %s" % (mesh0.num_cells(), Lambda0)
+            if i == 0:
+                mesh, Lambda = mesh0, set(Lambda0)
+            else:
+                mesh, _ = create_joint_mesh([mesh0, mesh])
+                Lambda = Lambda.union(set(Lambda0))
+    print "=== FINAL mesh has %i cells and Lambda is %s ===" % (mesh.num_cells(), Lambda)
+    return mesh, Lambda
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    generate_reference_setup('.')
